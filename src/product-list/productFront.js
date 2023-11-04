de55be2d7a79fd17d37f53bcac777cbe0ab00918
Aakash Mishra <aakashmishra.281@gmail.com>
2023-11-04 16:37:30 +0530

    filter button added

--- src/product-list/productFront.js
+++ src/product-list/productFront.js
@@ -3,15 +3,37 @@ import ProductList from "./productList";
 import Pagination from "./pagination";
 import styled from "styled-components";
 import products from "./styles/products_Data";
+import filter from "../assets/filter.png";
 
+const NavBar = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    color: #ECE3CE;
+    background: #0C356A;
+`;
+const FilterButton = styled.button`
+    padding: 8px;
+    margin-top: 15px;
+    margin-right: 100px;
+    background: white;
+    border-radius: 12px;
+    display: flex;
+    flex-direction: row;
+    gap: 4px;
+    width: fit-content;
+    height: fit-content;
+    align-items: center;
+    img {
+        height: 30px
+    }
+`;
 const Heading = styled.h1`
     margin: 0;
     padding: 10px;
     padding-left: 40px;
     font-size: 45px;
-    color: #ECE3CE;
     text-align: left;
-    background: #0C356A;
 `;
 
 const ProductFront = () => {
@@ -26,7 +48,13 @@ const ProductFront = () => {
 
     return (
         <React.Fragment>
+        <NavBar>
             <Heading>Product List</Heading>
+            <FilterButton onClick={() => console.log(productData.length)}>
+                <img src={filter} alt="filter" />
+                <h2>Filters</h2>
+            </FilterButton>
+        </NavBar>
             <Pagination
                 totalItems={productData.length}
                 postsPerPage={postsPerPage}
